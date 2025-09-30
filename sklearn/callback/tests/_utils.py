5ad5c3f890159119aefc5d43db01f3fa8300d4f6
François Paugam <35327799+FrancoisPgm@users.noreply.github.com>
2025-09-30 10:49:21 +0200

    use __skl_fit__ for callbacks estimators

--- sklearn/callback/tests/_utils.py
+++ sklearn/callback/tests/_utils.py
@@ -37,7 +37,37 @@ class NotValidCallback:
         pass  # pragma: no cover
 
 
-class Estimator(CallbackSupportMixin, BaseEstimator):
+class BaseEstimatorPrivateFit(BaseEstimator):
+    """A class that adds the implementation of a public and private fit method to the
+    BaseEstimator class.
+    """
+
+    @_fit_context(prefer_skip_nested_validation=False)
+    def fit(self, X_train=None, y_train=None, X_val=None, y_val=None):
+        if isinstance(self, CallbackSupportMixin):
+            callback_ctx = self.init_callback_context()
+            callback_ctx.eval_on_fit_begin(estimator=self)
+            try:
+                return self.__skl_fit__(
+                    X_train=X_train,
+                    y_train=y_train,
+                    X_val=X_val,
+                    y_val=y_val,
+                    callback_ctx=callback_ctx,
+                )
+            finally:
+                if callback_ctx is not None:
+                    callback_ctx.eval_on_fit_end(estimator=self)
+        else:
+            return self.__skl_fit__(
+                X_train=X_train,
+                y_train=y_train,
+                X_val=X_val,
+                y_val=y_val,
+            )
+
+
+class Estimator(CallbackSupportMixin, BaseEstimatorPrivateFit):
     """A class that mimics the behavior of an estimator.
 
     The iterative part uses a loop with a max number of iterations known in advance.
@@ -49,20 +79,23 @@ class Estimator(CallbackSupportMixin, BaseEstimator):
         self.max_iter = max_iter
         self.computation_intensity = computation_intensity
 
-    @_fit_context(prefer_skip_nested_validation=False)
-    def fit(self, X=None, y=None):
-        callback_ctx = self.init_callback_context(
-            max_subtasks=self.max_iter
-        ).eval_on_fit_begin(estimator=self)
-
+    def __skl_fit__(
+        self, X_train=None, y_train=None, X_val=None, y_val=None, callback_ctx=None
+    ):
         for i in range(self.max_iter):
-            subcontext = callback_ctx.subcontext(task_id=i)
+            if callback_ctx is not None:
+                subcontext = callback_ctx.subcontext(task_id=i)
 
             time.sleep(self.computation_intensity)  # Computation intensive task
 
-            if subcontext.eval_on_fit_task_end(
+            if callback_ctx is not None and subcontext.eval_on_fit_task_end(
                 estimator=self,
-                data={"X_train": X, "y_train": y},
+                data={
+                    "X_train": X_train,
+                    "y_train": y_train,
+                    "X_val": X_val,
+                    "y_val": y_val,
+                },
             ):
                 break
 
@@ -71,7 +104,7 @@ class Estimator(CallbackSupportMixin, BaseEstimator):
         return self
 
 
-class WhileEstimator(CallbackSupportMixin, BaseEstimator):
+class WhileEstimator(CallbackSupportMixin, BaseEstimatorPrivateFit):
     """A class that mimics the behavior of an estimator.
 
     The iterative part uses a loop with a max number of iterations known in advance.
@@ -82,19 +115,24 @@ class WhileEstimator(CallbackSupportMixin, BaseEstimator):
     def __init__(self, computation_intensity=0.001):
         self.computation_intensity = computation_intensity
 
-    @_fit_context(prefer_skip_nested_validation=False)
-    def fit(self, X=None, y=None):
-        callback_ctx = self.init_callback_context().eval_on_fit_begin(estimator=self)
-
+    def __skl_fit__(
+        self, X_train=None, y_train=None, X_val=None, y_val=None, callback_ctx=None
+    ):
         i = 0
         while True:
-            subcontext = callback_ctx.subcontext(task_id=i)
+            if callback_ctx is not None:
+                subcontext = callback_ctx.subcontext(task_id=i)
 
             time.sleep(self.computation_intensity)  # Computation intensive task
 
-            if subcontext.eval_on_fit_task_end(
+            if callback_ctx is not None and subcontext.eval_on_fit_task_end(
                 estimator=self,
-                data={"X_train": X, "y_train": y},
+                data={
+                    "X_train": X_train,
+                    "y_train": y_train,
+                    "X_val": X_val,
+                    "y_val": y_val,
+                },
             ):
                 break
 
@@ -106,7 +144,7 @@ class WhileEstimator(CallbackSupportMixin, BaseEstimator):
         return self
 
 
-class MetaEstimator(CallbackSupportMixin, BaseEstimator):
+class MetaEstimator(CallbackSupportMixin, BaseEstimatorPrivateFit):
     """A class that mimics the behavior of a meta-estimator.
 
     It has two levels of iterations. The outer level uses parallelism and the inner
@@ -124,21 +162,24 @@ class MetaEstimator(CallbackSupportMixin, BaseEstimator):
         self.n_jobs = n_jobs
         self.prefer = prefer
 
-    @_fit_context(prefer_skip_nested_validation=False)
-    def fit(self, X=None, y=None):
-        callback_ctx = self.init_callback_context(
-            max_subtasks=self.n_outer
-        ).eval_on_fit_begin(estimator=self)
-
+    def __skl_fit__(
+        self, X_train=None, y_train=None, X_val=None, y_val=None, callback_ctx=None
+    ):
         Parallel(n_jobs=self.n_jobs, prefer=self.prefer)(
             delayed(_func)(
                 self,
                 self.estimator,
-                X,
-                y,
+                data={
+                    "X_train": X_train,
+                    "y_train": y_train,
+                    "X_val": X_val,
+                    "y_val": y_val,
+                },
                 callback_ctx=callback_ctx.subcontext(
                     task_name="outer", task_id=i, max_subtasks=self.n_inner
-                ),
+                )
+                if callback_ctx is not None
+                else None,
             )
             for i in range(self.n_outer)
         )
@@ -146,22 +187,28 @@ class MetaEstimator(CallbackSupportMixin, BaseEstimator):
         return self
 
 
-def _func(meta_estimator, inner_estimator, X, y, *, callback_ctx):
+def _func(meta_estimator, inner_estimator, data, *, callback_ctx):
     for i in range(meta_estimator.n_inner):
         est = clone(inner_estimator)
 
-        inner_ctx = callback_ctx.subcontext(
-            task_name="inner", task_id=i
-        ).propagate_callbacks(sub_estimator=est)
-
-        est.fit(X, y)
-
-        inner_ctx.eval_on_fit_task_end(
-            estimator=meta_estimator,
-            data={"X_train": X, "y_train": y},
+        inner_ctx = (
+            callback_ctx.subcontext(task_name="inner", task_id=i).propagate_callbacks(
+                sub_estimator=est
+            )
+            if callback_ctx is not None
+            else None
         )
 
-    callback_ctx.eval_on_fit_task_end(
-        estimator=meta_estimator,
-        data={"X_train": X, "y_train": y},
-    )
+        est.fit(**data)
+
+        if callback_ctx is not None:
+            inner_ctx.eval_on_fit_task_end(
+                estimator=meta_estimator,
+                data=data,
+            )
+
+    if callback_ctx is not None:
+        callback_ctx.eval_on_fit_task_end(
+            estimator=meta_estimator,
+            data=data,
+        )
