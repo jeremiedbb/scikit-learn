e3f46e4c42ef7c2f2e3854230bb19b197ccb6793
François Paugam <francois.paugam@laposte.net>
2026-03-18 14:15:45 +0100

    fix tests on logged values and output types, use make_regression everywhere

--- sklearn/callback/tests/test_scoring_monitor.py
+++ sklearn/callback/tests/test_scoring_monitor.py
@@ -132,20 +132,19 @@ def custom_score(estimator, X, y):
     "on",
     ["train_set", "validation_set", "both"],
 )
-def test_scoring_monitor_logged_values(scoring, on):
+def test_logged_values(scoring, on):
     """Test that the correct values are logged with a simple estimator.
 
     This test only looks at the dict outputs from `get_logs`.
     """
     max_iter = 3
-    n_dim = 5
+    n_features = 5
     n_samples = 3
     estimator = MaxIterEstimator(max_iter=max_iter)
     callback = ScoringMonitor(on=on, scoring=scoring)
     estimator.set_callbacks(callback)
-    rng = np.random.RandomState(0)
-    X_train, y_train = rng.uniform(size=(n_dim, n_samples)), rng.uniform(size=n_dim)
-    X_val, y_val = rng.uniform(size=(n_dim, n_samples)), rng.uniform(size=n_dim)
+    X_train, y_train = make_regression(n_samples, n_features, random_state=0)
+    X_val, y_val = make_regression(n_samples, n_features, random_state=1)
 
     estimator.fit(X=X_train, y=y_train, X_val=X_val, y_val=y_val)
     log = callback.get_logs(as_frame=False, select="most_recent")
@@ -168,7 +167,7 @@ def test_scoring_monitor_logged_values(scoring, on):
     "on",
     ["train_set", "validation_set", "both"],
 )
-def test_scoring_monitor_logged_values_dataframe(scoring, on):
+def test_logged_values_dataframe(scoring, on):
     """Test that the correct values are logged with a simple estimator.
 
     This test only looks at the pandas dataframe outputs from `get_logs`.
@@ -176,18 +175,17 @@ def test_scoring_monitor_logged_values_dataframe(scoring, on):
     pd = pytest.importorskip("pandas")
 
     max_iter = 3
-    n_dim = 5
+    n_features = 5
     n_samples = 3
     estimator = MaxIterEstimator(max_iter=max_iter)
     callback = ScoringMonitor(on=on, scoring=scoring)
     estimator.set_callbacks(callback)
-    rng = np.random.RandomState(0)
-    X_train, y_train = rng.uniform(size=(n_dim, n_samples)), rng.uniform(size=n_dim)
-    X_val, y_val = rng.uniform(size=(n_dim, n_samples)), rng.uniform(size=n_dim)
+    X_train, y_train = make_regression(n_samples, n_features, random_state=0)
+    X_val, y_val = make_regression(n_samples, n_features, random_state=1)
 
     estimator.fit(X=X_train, y=y_train, X_val=X_val, y_val=y_val)
-    log_df = callback.get_logs(as_frame=True, select="most_recent")
 
+    log_df = callback.get_logs(as_frame=True, select="most_recent")
     expected_log, scoring_names = make_expected_output_MaxIterEstimator(
         max_iter, scoring, on, X_train, y_train, X_val, y_val
     )
@@ -199,6 +197,7 @@ def test_scoring_monitor_logged_values_dataframe(scoring, on):
             if col not in scoring_names and col != "on"
         ]
     )
+    assert len(log_df) == len(expected_log_df)
     assert np.array_equal(log_df.index.names, expected_log_df.index.names)
     assert log_df.equals(expected_log_df)
 
@@ -212,7 +211,7 @@ def test_scoring_monitor_logged_values_dataframe(scoring, on):
     "on",
     ["train_set", "validation_set", "both"],
 )
-def test_scoring_monitor_logged_values_meta_estimator(prefer, scoring, on):
+def test_logged_values_meta_estimator(prefer, scoring, on):
     """Test that the correct values are logged with a meta-estimator.
 
     This test only looks at the dict outputs from `get_logs`.
@@ -220,11 +219,10 @@ def test_scoring_monitor_logged_values_meta_estimator(prefer, scoring, on):
     n_outer = 3
     n_inner = 2
     max_iter = 4
-    n_dim = 5
+    n_features = 5
     n_samples = 3
-    rng = np.random.RandomState(0)
-    X_train, y_train = rng.uniform(size=(n_dim, n_samples)), rng.uniform(size=n_dim)
-    X_val, y_val = rng.uniform(size=(n_dim, n_samples)), rng.uniform(size=n_dim)
+    X_train, y_train = make_regression(n_samples, n_features, random_state=0)
+    X_val, y_val = make_regression(n_samples, n_features, random_state=1)
     callback = ScoringMonitor(on=on, scoring=scoring)
     est = MaxIterEstimator(max_iter=max_iter)
     est.set_callbacks(callback)
@@ -232,9 +230,11 @@ def test_scoring_monitor_logged_values_meta_estimator(prefer, scoring, on):
         est, n_outer=n_outer, n_inner=n_inner, n_jobs=2, prefer=prefer
     )
 
-    meta_est.fit(X=X_train, y=y_train, X_val=X_val, y_val=y_val)
-    log = callback.get_logs(as_frame=False, select="most_recent")
+    with config_context(enable_metadata_routing=True):
+        est.set_fit_request(X_val=True, y_val=True)
+        meta_est.fit(X=X_train, y=y_train, X_val=X_val, y_val=y_val)
 
+    log = callback.get_logs(as_frame=False, select="most_recent")
     expected_log, scoring_names = make_expected_output_MetaEstimator(
         n_outer, n_inner, max_iter, scoring, on, X_train, y_train, X_val, y_val
     )
@@ -257,7 +257,7 @@ def test_scoring_monitor_logged_values_meta_estimator(prefer, scoring, on):
     "on",
     ["train_set", "validation_set", "both"],
 )
-def test_scoring_monitor_logged_values_dataframe_meta_estimator(prefer, scoring, on):
+def test_logged_values_dataframe_meta_estimator(prefer, scoring, on):
     """Test that the correct values are logged with a meta-estimator.
 
     This test only looks at the pandas dataframe outputs from `get_logs`.
@@ -266,11 +266,10 @@ def test_scoring_monitor_logged_values_dataframe_meta_estimator(prefer, scoring,
     n_outer = 3
     n_inner = 2
     max_iter = 4
-    n_dim = 5
+    n_features = 5
     n_samples = 3
-    rng = np.random.RandomState(0)
-    X_train, y_train = rng.uniform(size=(n_dim, n_samples)), rng.uniform(size=n_dim)
-    X_val, y_val = rng.uniform(size=(n_dim, n_samples)), rng.uniform(size=n_dim)
+    X_train, y_train = make_regression(n_samples, n_features, random_state=0)
+    X_val, y_val = make_regression(n_samples, n_features, random_state=1)
     callback = ScoringMonitor(on=on, scoring=scoring)
     est = MaxIterEstimator(max_iter=max_iter)
     est.set_callbacks(callback)
@@ -278,13 +277,14 @@ def test_scoring_monitor_logged_values_dataframe_meta_estimator(prefer, scoring,
         est, n_outer=n_outer, n_inner=n_inner, n_jobs=2, prefer=prefer
     )
 
-    meta_est.fit(X=X_train, y=y_train, X_val=X_val, y_val=y_val)
-    logs = callback.get_logs(as_frame=False, select="most_recent")
+    with config_context(enable_metadata_routing=True):
+        est.set_fit_request(X_val=True, y_val=True)
+        meta_est.fit(X=X_train, y=y_train, X_val=X_val, y_val=y_val)
 
+    log_df = callback.get_logs(as_frame=True, select="most_recent")
     expected_log_dict, scoring_names = make_expected_output_MetaEstimator(
         n_outer, n_inner, max_iter, scoring, on, X_train, y_train, X_val, y_val
     )
-    log_df = callback.get_logs(as_frame=True, select="most_recent")
 
     expected_log_df = pd.DataFrame(expected_log_dict)
     expected_log_df = expected_log_df.set_index(
@@ -295,6 +295,7 @@ def test_scoring_monitor_logged_values_dataframe_meta_estimator(prefer, scoring,
         ]
     )
 
+    assert len(log_df) == len(expected_log_df)
     assert np.array_equal(log_df.index.names, expected_log_df.index.names)
     assert log_df.equals(expected_log_df)
 
@@ -304,6 +305,7 @@ def test_get_logs_output_type_no_pandas():
     estimator = MaxIterEstimator()
     callback = ScoringMonitor(scoring="neg_mean_squared_error")
     estimator.set_callbacks(callback)
+    X, y = make_regression(n_samples=100, n_features=2, random_state=0)
 
     empty_logs_all = callback.get_logs(select="all", as_frame=False)
     assert isinstance(empty_logs_all, dict)
@@ -311,8 +313,8 @@ def test_get_logs_output_type_no_pandas():
 
     empty_logs_most_recent = callback.get_logs(select="most_recent", as_frame=False)
 
-    estimator.fit()
-    estimator.fit()
+    estimator.fit(X, y)
+    estimator.fit(X, y)
 
     logs_all = callback.get_logs(select="all", as_frame=False)
     logs_most_recent = callback.get_logs(select="most_recent", as_frame=False)
@@ -330,6 +332,7 @@ def test_get_logs_output_type_pandas():
     estimator = MaxIterEstimator()
     callback = ScoringMonitor(scoring="neg_mean_squared_error")
     estimator.set_callbacks(callback)
+    X, y = make_regression(n_samples=100, n_features=2, random_state=0)
 
     empty_logs_all = callback.get_logs(select="all", as_frame=True)
     assert isinstance(empty_logs_all, dict)
@@ -339,8 +342,8 @@ def test_get_logs_output_type_pandas():
     assert isinstance(empty_logs_most_recent, pd.DataFrame)
     assert empty_logs_most_recent.empty
 
-    estimator.fit()
-    estimator.fit()
+    estimator.fit(X, y)
+    estimator.fit(X, y)
 
     logs_all = callback.get_logs(select="all", as_frame=True)
     assert isinstance(logs_all, dict)
