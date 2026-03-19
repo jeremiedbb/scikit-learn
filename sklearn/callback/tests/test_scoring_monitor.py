c76f6c56504be64a48a11af75391e0d65d896610
Jérémie du Boisberranger <jeremie@probabl.ai>
2026-03-19 14:39:11 +0100

    review logs tests

--- sklearn/callback/tests/test_scoring_monitor.py
+++ sklearn/callback/tests/test_scoring_monitor.py
@@ -20,65 +20,66 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils._metadata_requests import UnsetMetadataPassedError
 
 
-def make_expected_output_MaxIterEstimator(
-    max_iter, scoring, eval_on, X_train, y_train, X_val, y_val
-):
-    """Generate the expected dict output of a ScoringMonitor on a MaxIterEstimator."""
+def _get_scorer_and_names(scoring):
     scorer = check_scoring(None, scoring)
     if isinstance(scoring, str):
-        scoring_names = [scoring]
+        score_names = [scoring]
     elif callable(scoring):
-        scoring_names = ["score"]
+        score_names = ["score"]
     else:  # multi-scorer
-        scoring_names = list(scorer._scorers.keys())
+        score_names = list(scorer._scorers.keys())
+    return scorer, score_names
+
+
+def _make_dataframe(log, score_names):
+    pd = pytest.importorskip("pandas")
+    df = pd.DataFrame(log)
+    df = df.set_index(
+        [col for col in df.columns if col not in score_names + ["eval_on"]]
+    )
+    return df
+
+
+def _make_expected_output_MaxIterEstimator(
+    max_iter, scoring, as_frame, X_train, y_train, X_val, y_val
+):
+    """Generate the expected output of a ScoringMonitor on a MaxIterEstimator."""
+    scorer, score_names = _get_scorer_and_names(scoring)
     est_name = MaxIterEstimator.__name__
 
     expected_log = []
     for i in range(max_iter):
         fitted_est = MaxIterEstimator(max_iter=i + 1).fit()
-        if eval_on in ("train_set", "both"):
+
+        for eval_on in ("train_set", "validation_set"):
             log_item = {
                 f"0_{est_name}_fit": 0,
                 f"1_{est_name}_iteration": i,
-                "eval_on": "train_set",
+                "eval_on": eval_on,
             }
-            score = scorer(fitted_est, X_train, y_train)
-            if isinstance(score, dict):
-                log_item.update(score)
+
+            X, y = (X_train, y_train) if eval_on == "train_set" else (X_val, y_val)
+            scores = scorer(fitted_est, X, y)
+            if isinstance(scores, dict):
+                log_item.update(scores)
             else:
-                log_item[scoring_names[0]] = score
+                log_item[score_names[0]] = scores
             expected_log.append(log_item)
 
-        if eval_on in ("validation_set", "both"):
-            log_item = {
-                f"0_{est_name}_fit": 0,
-                f"1_{est_name}_iteration": i,
-                "eval_on": "validation_set",
-            }
-            score = scorer(fitted_est, X_val, y_val)
-            if isinstance(score, dict):
-                log_item.update(score)
-            else:
-                log_item[scoring_names[0]] = score
-            expected_log.append(log_item)
+    if as_frame:
+        expected_log = _make_dataframe(expected_log, score_names)
 
-    return expected_log, scoring_names
+    return expected_log, score_names
 
 
-def make_expected_output_MetaEstimator(
-    n_outer, n_inner, max_iter, scoring, eval_on, X_train, y_train, X_val, y_val
+def _make_expected_output_MetaEstimator(
+    n_outer, n_inner, max_iter, scoring, as_frame, X_train, y_train, X_val, y_val
 ):
-    """Generate the expected dict output of a ScoringMonitor on a MetaEstimator.
+    """Generate the expected output of a ScoringMonitor on a MetaEstimator.
 
     The sub-estimators are expected to be MaxIterEstimator.
     """
-    scorer = check_scoring(None, scoring)
-    if isinstance(scoring, str):
-        scoring_names = [scoring]
-    elif callable(scoring):
-        scoring_names = ["score"]
-    else:  # multi-scorer
-        scoring_names = list(scorer._scorers.keys())
+    scorer, score_names = _get_scorer_and_names(scoring)
     meta_est_name = MetaEstimator.__name__
     sub_est_name = MaxIterEstimator.__name__
 
@@ -86,37 +87,28 @@ def make_expected_output_MetaEstimator(
     for i_outer, i_inner in product(range(n_outer), range(n_inner)):
         for i_estimator_iteration in range(max_iter):
             fitted_est = MaxIterEstimator(max_iter=i_estimator_iteration + 1).fit()
-            if eval_on in ("train_set", "both"):
+
+            for eval_on in ("train_set", "validation_set"):
                 log_item = {
-                    "eval_on": "train_set",
+                    "eval_on": eval_on,
                     f"0_{meta_est_name}_fit": 0,
                     f"1_{meta_est_name}_outer": i_outer,
                     f"2_{meta_est_name}_inner|{sub_est_name}_fit": i_inner,
                     f"3_{sub_est_name}_iteration": i_estimator_iteration,
                 }
-                score = scorer(fitted_est, X_train, y_train)
-                if isinstance(score, dict):
-                    log_item.update(score)
+
+                X, y = (X_train, y_train) if eval_on == "train_set" else (X_val, y_val)
+                scores = scorer(fitted_est, X, y)
+                if isinstance(scores, dict):
+                    log_item.update(scores)
                 else:
-                    log_item[scoring_names[0]] = score
+                    log_item[score_names[0]] = scores
                 expected_log.append(log_item)
 
-            if eval_on in ("validation_set", "both"):
-                log_item = {
-                    "eval_on": "validation_set",
-                    f"0_{meta_est_name}_fit": 0,
-                    f"1_{meta_est_name}_outer": i_outer,
-                    f"2_{meta_est_name}_inner|{sub_est_name}_fit": i_inner,
-                    f"3_{sub_est_name}_iteration": i_estimator_iteration,
-                }
-                score = scorer(fitted_est, X_val, y_val)
-                if isinstance(score, dict):
-                    log_item.update(score)
-                else:
-                    log_item[scoring_names[0]] = score
-                expected_log.append(log_item)
+    if as_frame:
+        expected_log = _make_dataframe(expected_log, score_names)
 
-    return expected_log, scoring_names
+    return expected_log, score_names
 
 
 def custom_score(estimator, X, y):
@@ -124,82 +116,56 @@ def custom_score(estimator, X, y):
     return 0
 
 
-@pytest.mark.parametrize(
-    "scoring",
-    ["neg_mean_squared_error", ("neg_mean_squared_error", "r2"), custom_score],
-)
-@pytest.mark.parametrize(
-    "eval_on",
-    ["train_set", "validation_set", "both"],
-)
-def test_logged_values(scoring, eval_on):
-    """Test that the correct values are logged with a simple estimator.
-
-    This test only looks at the dict outputs from `get_logs`.
-    """
+@pytest.mark.parametrize("eval_on", ["train_set", "validation_set", "both"])
+def test_eval_on(eval_on):
     max_iter = 3
-    n_features = 5
-    n_samples = 3
-    estimator = MaxIterEstimator(max_iter=max_iter)
-    callback = ScoringMonitor(eval_on=eval_on, scoring=scoring)
-    estimator.set_callbacks(callback)
-    X_train, y_train = make_regression(n_samples, n_features, random_state=0)
-    X_val, y_val = make_regression(n_samples, n_features, random_state=1)
+    callback = ScoringMonitor(eval_on=eval_on, scoring="r2")
+    estimator = MaxIterEstimator(max_iter=max_iter).set_callbacks(callback)
+    X, y = make_regression(n_samples=100, n_features=2, random_state=0)
+    X, X_val, y, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    estimator.fit(X=X_train, y=y_train, X_val=X_val, y_val=y_val)
+    estimator.fit(X=X, y=y, X_val=X_val, y_val=y_val)
     log = callback.get_logs(as_frame=False, select="most_recent")
 
-    expected_log, scoring_names = make_expected_output_MaxIterEstimator(
-        max_iter, scoring, eval_on, X_train, y_train, X_val, y_val
-    )
-    assert len(log) == len(expected_log)
-    for i in range(len(log)):
-        assert set(log[i].keys()) == set(expected_log[i].keys())
-        for key, val in log[i].items():
-            assert val == expected_log[i][key]
+    if eval_on in ("train_set", "validation_set"):
+        assert len(log) == max_iter
+        assert all(entry["eval_on"] == eval_on for entry in log)
+    else:  # eval_on == "both"
+        train_log = [entry for entry in log if entry["eval_on"] == "train_set"]
+        val_log = [entry for entry in log if entry["eval_on"] == "validation_set"]
+        assert len(train_log) == len(val_log) == max_iter
 
 
 @pytest.mark.parametrize(
     "scoring",
     ["neg_mean_squared_error", ("neg_mean_squared_error", "r2"), custom_score],
 )
-@pytest.mark.parametrize(
-    "eval_on",
-    ["train_set", "validation_set", "both"],
-)
-def test_logged_values_dataframe(scoring, eval_on):
-    """Test that the correct values are logged with a simple estimator.
-
-    This test only looks at the pandas dataframe outputs from `get_logs`.
-    """
-    pd = pytest.importorskip("pandas")
+@pytest.mark.parametrize("as_frame", [True, False])
+def test_logged_values(scoring, as_frame):
+    """Test that the correct values are logged with a simple estimator."""
+    if as_frame:
+        pytest.importorskip("pandas")
 
     max_iter = 3
-    n_features = 5
-    n_samples = 3
-    estimator = MaxIterEstimator(max_iter=max_iter)
-    callback = ScoringMonitor(eval_on=eval_on, scoring=scoring)
-    estimator.set_callbacks(callback)
-    X_train, y_train = make_regression(n_samples, n_features, random_state=0)
-    X_val, y_val = make_regression(n_samples, n_features, random_state=1)
+    callback = ScoringMonitor(eval_on="both", scoring=scoring)
+    estimator = MaxIterEstimator(max_iter=max_iter).set_callbacks(callback)
+    X, y = make_regression(n_samples=100, n_features=2, random_state=0)
+    X, X_val, y, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    estimator.fit(X=X_train, y=y_train, X_val=X_val, y_val=y_val)
+    estimator.fit(X=X, y=y, X_val=X_val, y_val=y_val)
 
-    log_df = callback.get_logs(as_frame=True, select="most_recent")
-    expected_log, scoring_names = make_expected_output_MaxIterEstimator(
-        max_iter, scoring, eval_on, X_train, y_train, X_val, y_val
+    log = callback.get_logs(as_frame=as_frame, select="most_recent")
+    expected_log, _ = _make_expected_output_MaxIterEstimator(
+        max_iter, scoring, as_frame, X, y, X_val, y_val
     )
-    expected_log_df = pd.DataFrame(expected_log)
-    expected_log_df = expected_log_df.set_index(
-        [
-            col
-            for col in expected_log_df.columns
-            if col not in scoring_names and col != "eval_on"
-        ]
-    )
-    assert len(log_df) == len(expected_log_df)
-    assert np.array_equal(log_df.index.names, expected_log_df.index.names)
-    assert log_df.equals(expected_log_df)
+
+    if as_frame:
+        assert np.array_equal(log.index.names, expected_log.index.names)
+        assert log.equals(expected_log)
+    else:
+        assert all(
+            entry == expected_entry for entry, expected_entry in zip(log, expected_log)
+        )
 
 
 @pytest.mark.parametrize("prefer", ["processes", "threads"])
@@ -207,97 +173,46 @@ def test_logged_values_dataframe(scoring, eval_on):
     "scoring",
     ["neg_mean_squared_error", ("neg_mean_squared_error", "r2"), custom_score],
 )
-@pytest.mark.parametrize(
-    "eval_on",
-    ["train_set", "validation_set", "both"],
-)
-def test_logged_values_meta_estimator(prefer, scoring, eval_on):
-    """Test that the correct values are logged with a meta-estimator.
+@pytest.mark.parametrize("as_frame", [True, False])
+def test_logged_values_meta_estimator(prefer, scoring, as_frame):
+    """Test that the correct values are logged with a meta-estimator."""
+    if as_frame:
+        pytest.importorskip("pandas")
 
-    This test only looks at the dict outputs from `get_logs`.
-    """
-    n_outer = 3
-    n_inner = 2
-    max_iter = 4
-    n_features = 5
-    n_samples = 3
-    X_train, y_train = make_regression(n_samples, n_features, random_state=0)
-    X_val, y_val = make_regression(n_samples, n_features, random_state=1)
-    callback = ScoringMonitor(eval_on=eval_on, scoring=scoring)
-    est = MaxIterEstimator(max_iter=max_iter)
-    est.set_callbacks(callback)
+    n_outer, n_inner, max_iter = 3, 2, 5
+    callback = ScoringMonitor(eval_on="both", scoring=scoring)
+    est = MaxIterEstimator(max_iter=max_iter).set_callbacks(callback)
     meta_est = MetaEstimator(
         est, n_outer=n_outer, n_inner=n_inner, n_jobs=2, prefer=prefer
     )
+    X, y = make_regression(n_samples=100, n_features=2, random_state=0)
+    X, X_val, y, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
 
     with config_context(enable_metadata_routing=True):
         est.set_fit_request(X_val=True, y_val=True)
-        meta_est.fit(X=X_train, y=y_train, X_val=X_val, y_val=y_val)
+        meta_est.fit(X=X, y=y, X_val=X_val, y_val=y_val)
 
-    log = callback.get_logs(as_frame=False, select="most_recent")
-    expected_log, scoring_names = make_expected_output_MetaEstimator(
-        n_outer, n_inner, max_iter, scoring, eval_on, X_train, y_train, X_val, y_val
+    log = callback.get_logs(as_frame=as_frame, select="most_recent")
+    expected_log, score_names = _make_expected_output_MetaEstimator(
+        n_outer, n_inner, max_iter, scoring, as_frame, X, y, X_val, y_val
     )
 
-    assert len(log) == len(expected_log)
-    for i in range(len(log)):
-        assert set(log[i].keys()) == set(expected_log[i].keys())
-        for key, val in log[i].items():
-            # The log items might not be in the same order as the expected log because
-            # of the parallelization.
-            assert val in [expected_log[k][key] for k in range(len(log))]
-
-
-@pytest.mark.parametrize("prefer", ["processes", "threads"])
-@pytest.mark.parametrize(
-    "scoring",
-    ["neg_mean_squared_error", ("neg_mean_squared_error", "r2"), custom_score],
-)
-@pytest.mark.parametrize(
-    "eval_on",
-    ["train_set", "validation_set", "both"],
-)
-def test_logged_values_dataframe_meta_estimator(prefer, scoring, eval_on):
-    """Test that the correct values are logged with a meta-estimator.
-
-    This test only looks at the pandas dataframe outputs from `get_logs`.
-    """
-    pd = pytest.importorskip("pandas")
-    n_outer = 3
-    n_inner = 2
-    max_iter = 4
-    n_features = 5
-    n_samples = 3
-    X_train, y_train = make_regression(n_samples, n_features, random_state=0)
-    X_val, y_val = make_regression(n_samples, n_features, random_state=1)
-    callback = ScoringMonitor(eval_on=eval_on, scoring=scoring)
-    est = MaxIterEstimator(max_iter=max_iter)
-    est.set_callbacks(callback)
-    meta_est = MetaEstimator(
-        est, n_outer=n_outer, n_inner=n_inner, n_jobs=2, prefer=prefer
-    )
-
-    with config_context(enable_metadata_routing=True):
-        est.set_fit_request(X_val=True, y_val=True)
-        meta_est.fit(X=X_train, y=y_train, X_val=X_val, y_val=y_val)
-
-    log_df = callback.get_logs(as_frame=True, select="most_recent")
-    expected_log_dict, scoring_names = make_expected_output_MetaEstimator(
-        n_outer, n_inner, max_iter, scoring, eval_on, X_train, y_train, X_val, y_val
-    )
-
-    expected_log_df = pd.DataFrame(expected_log_dict)
-    expected_log_df = expected_log_df.set_index(
-        [
-            col
-            for col in expected_log_df.columns
-            if col not in scoring_names and col != "eval_on"
-        ]
-    )
-
-    assert len(log_df) == len(expected_log_df)
-    assert np.array_equal(log_df.index.names, expected_log_df.index.names)
-    assert log_df.equals(expected_log_df)
+    if as_frame:
+        assert np.array_equal(log.index.names, expected_log.index.names)
+        assert log.equals(expected_log)
+    else:
+        # The log items might not be in the same order as the expected log due to the
+        # parallelization of the meta-estimator. Hence we sort the log by the values of
+        # all keys but "eval_on" and the score names before comparing.
+        keys = tuple(log[0].keys() - {"eval_on"} - set(score_names))
+        sorted_log = sorted(log, key=lambda x: tuple(str(x[k]) for k in keys))
+        sorted_expected_log = sorted(
+            expected_log, key=lambda x: tuple(str(x[k]) for k in keys)
+        )
+        assert all(
+            entry == expected_entry
+            for entry, expected_entry in zip(sorted_log, sorted_expected_log)
+        )
 
 
 @pytest.mark.parametrize("as_frame", [True, False])
@@ -314,12 +229,9 @@ def test_get_logs_output_type_no_fit(as_frame):
     assert len(logs_all) == 0
 
     log_most_recent = callback.get_logs(select="most_recent", as_frame=as_frame)
-    if as_frame:
-        assert isinstance(log_most_recent, pd.DataFrame)
-        assert log_most_recent.empty
-    else:
-        assert isinstance(log_most_recent, list)
-        assert len(log_most_recent) == 0
+    expected_type = list if not as_frame else pd.DataFrame
+    assert isinstance(log_most_recent, expected_type)
+    assert len(log_most_recent) == 0
 
 
 @pytest.mark.parametrize("as_frame", [True, False])
@@ -352,7 +264,7 @@ def test_estimator_without_reconstruction_attributes():
     assert len(callback.get_logs()) == 0
 
 
-def test_sample_weights_and_metadata():
+def test_sample_weights_and_metadata_routing():
     """Check that the ScoringMonitor works with sample weights and metadata-routing.
 
     - passing sample weights results in a different score than not passing them.
